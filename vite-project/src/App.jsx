e903017791cdf8ec3af91adea5aca1e52f692d4e
loganwong52 <loganwong52@gmail.com>
2022-07-18 14:50:06 -0700

    added words.json file and displays blank spaces

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,32 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import * as Words from './data/words.json'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const randNum = Math.floor(Math.random() * Words.default.length)
+
+  const [word, setWord] = useState(Words.default[randNum])
+
+  // render empty spaces
+  const renderWord = () => {
+    let elements = []
+    for (let i = 0; i < word.length; i++) {
+      elements.push(
+        <span key={i} className="empty-space">{"_  "}</span>
+      )
+    }
+    return elements
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hangman!</h1>
+      <hr />
+      {renderWord()}
+
+      <br />
+      {word}
     </div>
   )
 }
